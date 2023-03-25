05ae40e9f275bb562071b4505f16b2e10262c82a
iruzo <hidden>
2023-03-25 22:23:59 +0100

    changed dots on nix config

--- .config/nix/configuration.nix
+++ .config/nix/configuration.nix
@@ -1,7 +1,7 @@
 # vm
-# sudo nix --extra-experimental-features nix-command --extra-experimental-features flakes run github:nix-community/disko -- --mode zap_create_mount dotfiles/.config/nix/laptop/disko.nix --arg disks '[ "/dev/vda" ]' && sudo nixos-generate-config --root /mnt && sudo cp -r dotfiles/.config/nix/* /mnt/etc/nixos/
+# sudo nix --extra-experimental-features nix-command --extra-experimental-features flakes run github:nix-community/disko -- --mode zap_create_mount dots/.config/nix/laptop/disko.nix --arg disks '[ "/dev/vda" ]' && sudo nixos-generate-config --root /mnt && sudo cp -r dots/.config/nix/* /mnt/etc/nixos/
 # laptop
-# sudo nix --extra-experimental-features nix-command --extra-experimental-features flakes run github:nix-community/disko -- --mode zap_create_mount dotfiles/.config/nix/laptop/disko.nix --arg disks '[ "/dev/nvme0n1" ]' && sudo nixos-generate-config --root /mnt && sudo cp -r dotfiles/.config/nix/* /mnt/etc/nixos/
+# sudo nix --extra-experimental-features nix-command --extra-experimental-features flakes run github:nix-community/disko -- --mode zap_create_mount dots/.config/nix/laptop/disko.nix --arg disks '[ "/dev/nvme0n1" ]' && sudo nixos-generate-config --root /mnt && sudo cp -r dots/.config/nix/* /mnt/etc/nixos/
 
 # REMEMBER TO UNCOMMENT THE CONFIG
 
