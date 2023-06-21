98e35719333671633c68df64c870a2c8c081f7f4
iruzo <hidden>
2023-06-21 11:48:21 +0200

    testing changes on nixos vm config

--- .config/nix/install.sh
+++ .config/nix/install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sudo nix --extra-experimental-features nix-command --extra-experimental-features flakes run github:nix-community/disko -- --mode zap_create_mount $HOME/dots/.config/nix/laptop/disko.nix --arg disks '[ "/dev/vda" ]' \
+sudo nix --extra-experimental-features nix-command --extra-experimental-features flakes run github:nix-community/disko -- --mode disko $HOME/dots/.config/nix/vm/disko.nix --arg disks '[ "/dev/vda" ]' \
 && sudo nixos-install --no-write-lock-file --flake https://github.com/iruzo/dots#vm
--- .config/nix/vm/disko.nix
+++ .config/nix/vm/disko.nix
@@ -1,32 +1,45 @@
 { disks ? [ "/dev/vda" ], ... }: {
   disko.devices = {
     disk = {
-      vda = {
-        device = builtins.elemAt disks 0;
+      main = {
         type = "disk";
+        device = builtins.elemAt disks 0;
         content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              type = "EF00";
-              size = "100M";
+          type = "table";
+          format = "gpt";
+          partitions = [
+            {
+              name = "boot";
+              start = "0";
+              end = "1M";
+              flags = [ "bios_grub" ];
+            }
+            {
+              name = "ESP";
+              start = "1M";
+              end = "512M";
+              bootable = true;
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
               };
-            };
-            root = {
-              size = "100%";
+            }
+            {
+              name = "root";
+              start = "512M";
+              end = "100%";
               content = {
                 type = "filesystem";
                 format = "ext4";
                 mountpoint = "/";
               };
-            };
-          };
+            }
+          ];
         };
       };
     };
   };
 }
+
+
