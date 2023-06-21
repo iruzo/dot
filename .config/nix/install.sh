09d94289bc2d0c21ba3c93e54ff2ea640a3a5800
iruzo <hidden>
2023-06-21 10:39:15 +0200

    added param to install.sh for nix

--- .config/nix/install.sh
+++ .config/nix/install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 sudo nix --extra-experimental-features nix-command --extra-experimental-features flakes run github:nix-community/disko -- --mode zap_create_mount $HOME/dots/.config/nix/laptop/disko.nix --arg disks '[ "/dev/vda" ]' \
-&& sudo nixos-install --flake https://github.com/iruzo/dots#vm
+&& sudo nixos-install --no-write-lock-file --flake https://github.com/iruzo/dots#vm
