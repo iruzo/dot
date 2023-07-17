faf4a5106e71b402c375684d144010e57e81eb80
iruzo <dev@iruzo.dev>
2023-07-18 00:10:37 +0200

    removed null-ls since it will be deprecated soon

--- .config/nvim/lua/plug.lua
+++ .config/nvim/lua/plug.lua
@@ -375,21 +375,6 @@ return {
   },
 
   {
-    'jose-elias-alvarez/null-ls.nvim',
-    event = { 'BufReadPre', 'BufNewFile' },
-    config = function()
-      local null_ls = require('null-ls')
-      null_ls.setup({
-        sources = {
-          null_ls.builtins.formatting.stylua,
-          null_ls.builtins.diagnostics.eslint,
-          null_ls.builtins.completion.spell,
-        },
-      })
-    end
-  },
-
-  {
     'neovim/nvim-lspconfig',
     tag = 'v0.1.6',
     event = { 'BufReadPre', 'BufNewFile' },
