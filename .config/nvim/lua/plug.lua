d78709a527e4e2ccbe1c0fc9746a379878b64d9c
iruzo <dev@iruzo.dev>
2023-07-19 17:40:21 +0200

    fixed luasnip cmp problems in nvim config

--- .config/nvim/lua/plug.lua
+++ .config/nvim/lua/plug.lua
@@ -451,6 +451,9 @@ return {
                 dependencies = {
                   'rafamadriz/friendly-snippets',
                 },
+                config = function()
+                  require'luasnip.loaders.from_vscode'.lazy_load()
+                end,
               }
             }
           },
