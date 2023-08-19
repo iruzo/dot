6aa55e4a106ead9ab6646db62ad254aa52a35a91
ruzo <dev@ruzo.sh>
2023-08-19 17:26:17 +0200

    add: session env vars

--- .config/nix/usr.nix
+++ .config/nix/usr.nix
@@ -29,6 +29,21 @@
 
   users.defaultUserShell = pkgs.dash;
 
+  environment.sessionVariables = rec {
+    GTK_THEME="Adwaita:dark";
+
+    XDG_CACHE_HOME  = "$HOME/.cache";
+    XDG_CONFIG_HOME = "$HOME/.config";
+    XDG_DATA_HOME   = "$HOME/.local/share";
+    XDG_STATE_HOME  = "$HOME/.local/state";
+
+    # Not officially in the specification
+    XDG_BIN_HOME    = "$HOME/.local/bin";
+    PATH = [
+      "${XDG_BIN_HOME}"
+    ];
+  };
+
   users.users.ruzo = {
     isNormalUser = true;
     shell = pkgs.yash;
