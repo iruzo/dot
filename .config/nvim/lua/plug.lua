91c726fa42c3295efeb694be5e7a4fba39e69b35
ruzo <dev@ruzo.sh>
2023-08-29 14:05:26 +0200

    ref: nvim config - changed lualine config

--- .config/nvim/lua/plug.lua
+++ .config/nvim/lua/plug.lua
@@ -212,6 +212,22 @@ return {
     },
     event = 'VeryLazy',
     opts = true,
+    config = function()
+      require'lualine'.setup ({
+        options = {
+          component_separators = { left = ' ', right = ' '},
+          section_separators = { left = '', right = ''},
+        },
+        sections = {
+          lualine_a = {'mode'},
+          lualine_b = {'branch', 'diff', 'diagnostics'},
+          lualine_c = {'fileformat', 'encoding', 'filetype', 'filename', 'filesize'},
+          lualine_x = {'selectioncount'},
+          lualine_y = {'progress'},
+          lualine_z = {'location'}
+        }
+      })
+    end
   },
 
   {
