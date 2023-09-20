5574ace7efeaa69080ddd96e1507c474b721b017
ruzo <dev@ruzo.sh>
2023-09-21 01:12:31 +0200

    ref: nix - changed sway config

--- .config/nix/laptop/gui/sway.nix
+++ .config/nix/laptop/gui/sway.nix
@@ -1,58 +1,21 @@
 { config, pkgs, lib, ... }:
 
-let
-  dbus-sway-environment = pkgs.writeTextFile {
-    name = "dbus-sway-environment";
-    destination = "/bin/dbus-sway-environment";
-    executable = true;
-
-    text = ''
-      dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
-      systemctl --user stop pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
-      systemctl --user start pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
-    '';
-  };
-
-  configure-gtk = pkgs.writeTextFile {
-    name = "configure-gtk";
-    destination = "/bin/configure-gtk";
-    executable = true;
-    text = let
-      schema = pkgs.gsettings-desktop-schemas;
-      datadir = "${schema}/share/gsettings-schemas/${schema.name}";
-    in ''
-      export XDG_DATA_DIRS=${datadir}:$XDG_DATA_DIRS
-      gnome_schema=org.gnome.desktop.interface
-      gsettings set $gnome_schema gtk-theme 'Dracula'
-    '';
-  };
-
-in
 {
 
-  programs.sway = {
-    enable = true;
-    package = (pkgs.swayfx.overrideAttrs
-      (old: {
-        passthru.providedSessions = [ "sway" ];
-      }));
-    # extraOptions = [ "--unsupported-gpu" ];
-    wrapperFeatures.gtk = true;
-    extraPackages = with pkgs; [
-      swaynotificationcenter
-      swaylock
-      wl-clipboard
-      bemenu
-      grim
-      slurp
-      brightnessctl
+  environment.systemPackages = with pkgs; [
+    swayfx
+    swaynotificationcenter
+    swaylock
+    wl-clipboard
+    bemenu
+    grim
+    slurp
+    brightnessctl
 
-      pcmanfm
-      gnome.gnome-logs
-      gnome.gnome-boxes
-    ];
-  };
-  # programs.xwayland.enable = true;
+    pcmanfm
+    gnome.gnome-logs
+    gnome.gnome-boxes
+  ];
   services.gvfs.enable = true;        # needed for pcmanfm
   services.udisks2.enable = true;     # needed for gnome-disks
   programs.gnome-disks.enable = true; # gnome-disks
@@ -60,7 +23,7 @@ in
   xdg.portal = {
     enable = true;
     wlr.enable = true;
-    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    # extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
   };
 
   # Enable Pipewire
