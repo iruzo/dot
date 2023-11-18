f75a39774735f4683e45ad6caf8247ef182e23af
ruzo <dev@ruzo.sh>
2023-11-18 15:59:40 +0100

    add: nvim - added endwise and autotag plugins

--- .config/nvim/lua/plug/treesitter.lua
+++ .config/nvim/lua/plug/treesitter.lua
@@ -5,6 +5,12 @@ return {
     tag = 'v0.9.1',
     dependencies = {
       'nvim-treesitter/nvim-treesitter-context',
+      'RRethy/nvim-treesitter-endwise',
+      {
+        'windwp/nvim-ts-autotag',
+        main = "nvim-ts-autotag",
+        opts = {},
+      },
     },
     build = ':TSUpdate',
     event = { 'BufReadPost', 'BufNewFile' },
@@ -12,6 +18,8 @@ return {
     config = function()
       require'nvim-treesitter.configs'.setup {
         auto_install = true,
+        auto_tag = { enable = true, },
+        endwise = { enable = true, },
         highlight = { enable = true, use_languagetree = true, additional_vim_regex_highlighting = false },
         indent = { enable = true },
       }
