1862c823b6580dbc122b596a40e31824c06a1665
ruzo <dev@ruzo.sh>
2023-12-01 18:44:30 +0100

    add: nvim - plug harpoon

---
+++ .config/nvim/lua/plug/harpoon.lua
@@ -0,0 +1,19 @@
+return {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = {
+      { 'nvim-lua/plenary.nvim', tag = 'v0.1.4', },
+      { 'nvim-telescope/telescope.nvim', tag = '0.1.4', },
+    },
+    config = function()
+      local harpoon = require('harpoon')
+      harpoon:setup()
+      vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+      vim.keymap.set('n', '<leader>p', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+      vim.keymap.set('n', '<leader>1', function() harpoon:list():select(1) end)
+      vim.keymap.set('n', '<leader>2', function() harpoon:list():select(2) end)
+      vim.keymap.set('n', '<leader>3', function() harpoon:list():select(3) end)
+      vim.keymap.set('n', '<leader>4', function() harpoon:list():select(4) end)
+      vim.keymap.set('n', '<leader>5', function() harpoon:list():select(5) end)
+    end
+}
