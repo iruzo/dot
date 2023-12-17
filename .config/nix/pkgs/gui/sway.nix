0a1501b1e59ddead17c3a2c61f90d6606caac647
ruzo <dev@ruzo.sh>
2023-12-18 00:24:13 +0100

    add: nix - added thunar, greetd and tlp on sway config

--- .config/nix/pkgs/gui/sway.nix
+++ .config/nix/pkgs/gui/sway.nix
@@ -1,5 +1,20 @@
 { config, pkgs, lib, ... }:
 
+# wallpaper
+# set $wallpaper https://raw.githubusercontent.com/iruzo/wp/main/original-anime-cyberpunk-edgerunners.png
+# exec_always if [  ! -f ~/.local/share/sway/wallpaper ]; then $(mkdir -p ~/.local/share/sway/wallpaper && curl $wallpaper -o ~/.local/share/sway/wallpaper); && sleep 1 fi
+# output * bg ~/.local/share/sway/wallpaper fill
+let
+  swayConfig = pkgs.writeText "greetd-sway-config" ''
+    exec "GTK_THEME='Adwaita:dark' ${pkgs.greetd.gtkgreet}/bin/gtkgreet -l; swaymsg exit; pkill wvkbd"
+    exec "${pkgs.wvkbd}/bin/wvkbd-mobintl -l simple,special"
+    bindsym Mod4+shift+e exec swaynag \
+      -t warning \
+      -m 'What do you want to do?' \
+      -b 'Poweroff' 'systemctl poweroff' \
+      -b 'Reboot' 'systemctl reboot'
+  '';
+in
 {
 
   programs.sway = {
@@ -15,18 +30,47 @@
       grim
       slurp
       brightnessctl
+      wvkbd
 
-      gnome.nautilus
+      # gnome.nautilus
       gnome.gnome-logs
       gnome.gnome-boxes
+
+      greetd.gtkgreet
+
+      # egl-wayland # needed for nvidia
     ];
   };
-  services.gvfs.enable = true;        # needed for nautilus and pcmanfm
-  services.udisks2.enable = true;     # needed for gnome-disks
-  programs.gnome-disks.enable = true; # gnome-disks
-
-  services.tlp.enable = true;           # performance
-  # services.auto-cpufreq.enable = true;  # performance
+  programs = {
+    thunar = {
+      enable = true;
+      plugins = with pkgs.xfce; [
+        thunar-archive-plugin
+        thunar-volman
+      ];
+    };
+    gnome-disks.enable = true; # gnome-disks
+  };
+  services = {
+    gvfs.enable = true;         # needed for nautilus, pcmanfm and thunar to mount devices
+    udisks2.enable = true;      # needed for gnome-disks
+    tlp.enable = true;          # performance
+    # auto-cpufreq.enable = true; # performance
+    greetd = {
+      enable = true;
+      settings = {
+        default_session = {
+          command = ''
+            ${pkgs.sway}/bin/sway --config ${swayConfig}
+            '';
+        };
+      };
+    };
+  };
+  environment.etc."greetd/environments".text = ''
+    sway
+    sh
+  '';
 
   services.dbus.enable = true;
   xdg.portal = {
