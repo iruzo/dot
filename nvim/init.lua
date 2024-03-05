581cc69bc820545e42dbbc2ec5b111112ab22997
ruzo <dev@ruzo.sh>
2024-03-05 11:21:39 +0100

    init

---
+++ nvim/init.lua
@@ -0,0 +1,33 @@
+require("core/config")
+require("core/keys")
+require("core/gui")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup(
+  {
+    { import="plug" },
+  },
+  {
+    checker= {
+      enabled=true,
+      notify=false,
+    },
+    change_detection = {
+      notify=false,
+    },
+  }
+)
